828c5fbe826e3707ec8f58be731336b69258fbac
Yago <y.felixdasilva@gmail.com>
2025-08-28 15:45:11 -0300

    feat: complete login form

--- vite-project/src/Components/Login/Login.jsx
+++ vite-project/src/Components/Login/Login.jsx
@@ -1,19 +1,39 @@
 import {FaUser, FaLock} from 'react-icons/fa';
 
+import { useState } from 'react';
 
+import './Login.css';
 
 const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        alert('Enviando os dados:' + username + ' - ' + password)
+
+        /*console.log('Envio');*/
+    }
+
   return (
     <div className="container">
-        <form>
+        <form onSubmit={handleSubmit}>
             <h1>Acesse o sistema</h1>
             <div>
-                <input type="email" placeholder='E-mail' />
-                <FaUser className='icon' />
+                <input 
+                    type="email" 
+                    placeholder='E-mail'
+                    required 
+                    onChange={(e) => setUsername(e.target.value)} />
+                    <FaUser className='icon' />
             </div>
             <div>
-                <input type="password" placeholder='Senha' />
-                <FaLock className='icon' />
+                <input 
+                    type="password" 
+                    placeholder='Senha' 
+                    onChange={(e) => setPassword(e.target.value)} />
+                    <FaLock className='icon' />
             </div>
 
             <div className="recall-forget">
