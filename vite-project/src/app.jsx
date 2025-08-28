a1539a199c096d06b0ca62b76e42490de71fcdeb
Yago <y.felixdasilva@gmail.com>
2025-08-28 11:29:58 -0300

    feat: login form

---
+++ vite-project/src/app.jsx
@@ -0,0 +1,13 @@
+              
+import './app.css'
+import Login from './Components/Login/Login';
+
+export function App() {
+
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
